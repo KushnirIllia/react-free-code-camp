ae820cf06a2e6420b4db211a98cfb92622830456
KushnirIllia <illiakushnira@gmail.com>
2023-01-07 22:48:52 +0200

    Learning reactDom and render methods

--- 05/src/index.js
+++ 05/src/index.js
@@ -4,14 +4,22 @@ import "./index.css"
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-const navbar = (
-	<nav>
-		<h1>Kush Corp</h1>
-		<ul>
-			<li>Menu</li>
-			<li>About</li>
-			<li>Contact</li>
-		</ul>
-	</nav>
+const page = (
+	<div>
+		<header>
+			<nav>
+				<h1>Kush Corp</h1>
+				<ul>
+					<li>Menu</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<p>Main content</p>
+		</main>
+		<footer>all is copyright</footer>
+	</div>
 )
-root.render(navbar)
+root.render(page)
